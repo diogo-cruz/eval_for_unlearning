177009326a7b67d85ce9f897af5b493d482fbcc9
Yeonwoo Jang <yjang385@gmail.com>
2025-03-24 05:14:11 +0000

    move outside of wmdp folder

---
+++ evaluation/rephrase_prompts.py
@@ -0,0 +1,171 @@
+import os
+import re
+import json
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+PROMPT_PATH = Path(__file__).parent / "prompts"
+DATA_PATH = Path(__file__).parent.parent / "data"
+
+def _load_prompt_settings() -> dict:
+  with open(PROMPT_PATH / "settings.json", "r") as f:
+    return json.load(f)
+
+def get_prompt_technique_names() -> list[str]:
+  return list(_load_prompt_settings().keys())
+
+def load_prompt_settings(prompt_technique_name: str) -> dict:
+  return _load_prompt_settings()[prompt_technique_name]
+
+SAVE_DIR_MAPPING = {}
+for prompt_technique_name in _load_prompt_settings():
+  if prompt_technique_name == "rephrase_as_conversation":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_rephrased_conversation"
+  elif prompt_technique_name == "rephrase_as_poem":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_rephrased_poem"
+  elif prompt_technique_name == "removing_technical_terms_from_the_question":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_technical_terms_removed_1"
+  elif prompt_technique_name == "removing_technical_terms_from_both_the_question_and_the_choices":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_technical_terms_removed_2"
+  elif prompt_technique_name == "translating_to_language":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_translated_"
+  elif prompt_technique_name == "replacing_technical_terms_with_variables":
+    SAVE_DIR_MAPPING[prompt_technique_name] = "data_replaced_with_variables"
+  else:
+    raise ValueError(f"Invalid prompt technique name: {prompt_technique_name}")
+
+
+def load_prompt(prompt_technique_name: str) -> str:
+  prompt_path = PROMPT_PATH / f"{prompt_technique_name}"
+  with open(prompt_path, "r") as f:
+    return f.read().strip()
+
+def load_wmdp_data(task: str) -> list[dict]:
+  res = []
+  with open(DATA_PATH / "wmdp" / f"{task}_questions.json", "r") as f:
+    for line in f:
+      line = json.loads(line)
+      res.append(line)
+  return res
+
+def load_tiny_mmlu_data(subject: str) -> list[dict]:
+  res = pd.read_csv(DATA_PATH / "tinyMMLU" / "dev" / f"{subject}_dev.csv", header=None)
+  return res
+
+def extract_tags(text):
+  pattern = r'<([^>]+)>'
+  matches = re.findall(pattern, text)
+  return matches
+
+def generate_rephrased_prompt(prompt_technique_name: str, question: str, **kwargs):
+  # assert "model" in kwargs, f"model not in kwargs: {kwargs}"
+  # assert "max_tokens" in kwargs, f"max_tokens not in kwargs: {kwargs}"
+  # assert "temperature" in kwargs, f"temperature not in kwargs: {kwargs}"
+
+  prompt = load_prompt(prompt_technique_name)
+  prompt = prompt.replace("<question>", question)
+  tags = extract_tags(prompt)
+  for tag in tags:
+    if tag == "question": continue
+    assert tag in kwargs, f"tag {tag} not in kwargs: {kwargs}"
+    prompt = prompt.replace(f"<{tag}>", kwargs[tag])
+
+  try:
+    client = Anthropic()
+    settings = load_prompt_settings(prompt_technique_name)
+    if "languages" in settings:
+      assert kwargs["language"] in settings["languages"], f"invalid language: {kwargs['language']}"
+      del settings["languages"]
+
+    message = client.messages.create(
+      messages=[{"role": "user", "content": prompt}],
+      **settings
+    )
+    return message.content[0].text
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
+def generate_rephrased_wmdp_data(task: str, prompt_technique_name: str, verbose: bool = False, **kwargs):
+  assert task in ["bio", "cyber"], f"invalid task: {task}"
+
+  lines = load_wmdp_data(task)
+  for i, line in tqdm(enumerate(lines), total=len(lines)):
+    question = line["question"]
+    if verbose: print("original question:", question)
+    rephrased_question = generate_rephrased_prompt(prompt_technique_name, question, **kwargs)
+    if verbose: print("rephrased question:", rephrased_question)
+    
+    ## overwrite the question
+    lines[i]["question"] = rephrased_question
+    lines[i]["original_question"] = question
+
+    if i == 0:
+      print(question)
+      print(rephrased_question)
+
+  ## save the original questions
+  wmdp_rephrased_data_dir = DATA_PATH / "wmdp-rephrased"
+  Path(wmdp_rephrased_data_dir).mkdir(parents=True, exist_ok=True)
+  
+  # TODO: add language to the file name (if available)
+  with open(wmdp_rephrased_data_dir / f"{task}_questions_{prompt_technique_name}.json", "w") as f:
+    for line in lines:
+      f.write(json.dumps(line) + "\n")
+
+def generate_rephrased_tiny_mmlu_data(subject: str, prompt_technique_name: str, verbose: bool = False, **kwargs):
+  lines = load_tiny_mmlu_data(subject=subject)
+  for i, line in tqdm(lines.iterrows(), total=len(lines)):
+    question = line[0]
+    if verbose: print("original question:", question)
+    rephrased_question = generate_rephrased_prompt(prompt_technique_name, question, **kwargs)
+    if verbose: print("rephrased question:", rephrased_question)
+
+    ## overwrite the question
+    lines.loc[i,0] = rephrased_question
+
+    if i == 0:
+      print(question)
+      print(rephrased_question)
+
+  ## save the original questions
+  tiny_mmlu_rephrased_data_dir = DATA_PATH / "tinyMMLU-rephrased" / SAVE_DIR_MAPPING[prompt_technique_name]
+  if "language" in kwargs:
+    tiny_mmlu_rephrased_data_dir = tiny_mmlu_rephrased_data_dir.parent / (tiny_mmlu_rephrased_data_dir.name + kwargs["language"].lower()) / "dev"
+  else:
+    tiny_mmlu_rephrased_data_dir /= "dev"
+  Path(tiny_mmlu_rephrased_data_dir).mkdir(parents=True, exist_ok=True)
+
+  lines.to_csv(tiny_mmlu_rephrased_data_dir / f"{subject}_dev.csv", header=False, index=False)
+
+
+if __name__ == "__main__":
+  
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--data", type=str, required=True)
+  parser.add_argument("--task", type=str, choices=["bio", "cyber"])
+  parser.add_argument("--prompt_technique_name", type=str, required=True)
+  parser.add_argument("--language", type=str)
+  args = parser.parse_args()
+
+  print(f"Generating rephrases for {args.prompt_technique_name}")
+  
+  if args.data == "wmdp":
+    if args.language:
+      generate_rephrased_wmdp_data(task=args.task, prompt_technique_name=args.prompt_technique_name, language=args.language) 
+    else:
+      generate_rephrased_wmdp_data(task=args.task, prompt_technique_name=args.prompt_technique_name) 
+  elif args.data == "tinyMMLU":
+    from MMLU.tinyMMLU_utils import TASKS
+    for subject in TASKS:
+      if args.language:
+        generate_rephrased_tiny_mmlu_data(subject=subject, prompt_technique_name=args.prompt_technique_name, language=args.language)
+      else:
+        generate_rephrased_tiny_mmlu_data(subject=subject, prompt_technique_name=args.prompt_technique_name)
+  else:
+    raise ValueError(f"Invalid data argument: {args.data}")
