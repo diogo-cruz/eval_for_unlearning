9b2ee440a8f17eece157deb9198efabd0402f9a2
Yeonwoo Jang <yjang385@gmail.com>
2025-04-01 05:50:13 +0000

    remove tokens

--- run.sh
+++ run.sh
@@ -7,45 +7,31 @@ if [ ! -d "lm-evaluation-harness" ]; then
   cd ..
 fi
 
+if [ ! -d "lm-evaluation-harness/lm_eval/tasks/wmdp_rephrased" ]; then
+  echo "Copying wmdp_rephrased tasks to lm-evaluation-harness..."
+  mkdir -p lm-evaluation-harness/lm_eval/tasks/wmdp_rephrased
+  cp -r wmdp_lm_eval_tasks/* lm-evaluation-harness/lm_eval/tasks/wmdp_rephrased
+fi
 
-# below creates a log file
-# lm_eval \
-#   --model hf \
-#   --model_args pretrained=HuggingFaceH4/zephyr-7b-beta \
-#   --tasks wmdp \
-#   --limit 10 \
-#   --output output/wmdp/ \
-#   --log_samples
 
-# python3 -m eval.eval --model zephyr_7b_beta --tasks wmdp_bio wmdp_cyber wmdp_chem --output output/wmdp/ --log_samples
-# python3 -m eval.eval --model zephyr_7b_beta --tasks tinyMMLU --output output/tinyMMLU/ --log_samples
 
 export LOGLEVEL=DEBUG
-
-# lm_eval --model hf --model_args pretrained=HuggingFaceH4/zephyr-7b-beta --tasks wmdp_bio_rephrased --log_samples --output output/wmdp_rephrased
-# lm_eval --model hf --model_args pretrained=cais/zephyr_rmu --tasks wmdp_bio_rephrased --log_samples --output output/wmdp_rephrased
-
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_english_filler --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_hindi_filler --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_latin_filler --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_conversation --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_poem --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_replace_with_variables --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_technical_terms_removed_1 --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_arabic --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_bengali --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_czech --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_farsi --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_french --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_german --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_hindi --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_korean --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_telugu --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_turkish --output output/wmdp_rephrased --log_samples
-# python3 -m eval.eval --model zephyr_7b_rmu --tasks wmdp_bio_rephrased_translated_vietnamese --output output/wmdp_rephrased --log_samples
+# example below creates a log file
+# lm_eval --model hf --model_args pretrained=HuggingFaceH4/zephyr-7b-beta --tasks wmdp --limit 10 --output output/wmdp/ --log_samples
 
 # python3 -m eval.eval \
 #   --model zephyr_7b_beta \
 #   --tasks wmdp_bio wmdp_bio_rephrased_english_filler wmdp_bio_rephrased_hindi_filler wmdp_bio_rephrased_latin_filler wmdp_bio_rephrased_conversation wmdp_bio_rephrased_poem wmdp_bio_rephrased_replace_with_variables wmdp_bio_rephrased_technical_terms_removed_1 wmdp_bio_rephrased_translated_arabic wmdp_bio_rephrased_translated_bengali wmdp_bio_rephrased_translated_czech wmdp_bio_rephrased_translated_farsi wmdp_bio_rephrased_translated_french wmdp_bio_rephrased_translated_german wmdp_bio_rephrased_translated_hindi wmdp_bio_rephrased_translated_korean wmdp_bio_rephrased_translated_telugu wmdp_bio_rephrased_translated_turkish wmdp_bio_rephrased_translated_vietnamese \
 #   --output output/wmdp_rephrased \
 #   --log_samples
+
+source .env
+HF_TOKEN=$HF_TOKEN
+# MODEL="mistral_7b_v0.1"
+MODEL="llama3_8b_instruct_elm"
+# llama3_8b_elm
+# llama3_8b
+
+python3 -m eval.eval --model $MODEL --tasks wmdp_bio --output output/wmdp --log_samples --hf_token $HF_TOKEN
+python3 -m eval.eval --model $MODEL --output output/wmdp_rephrased --log_samples --hf_token $HF_TOKEN \
+  --tasks wmdp_bio_rephrased_english_filler wmdp_bio_rephrased_hindi_filler wmdp_bio_rephrased_latin_filler wmdp_bio_rephrased_conversation wmdp_bio_rephrased_poem wmdp_bio_rephrased_replace_with_variables wmdp_bio_rephrased_technical_terms_removed_1 wmdp_bio_rephrased_translated_farsi  wmdp_bio_rephrased_translated_german wmdp_bio_rephrased_translated_korean
